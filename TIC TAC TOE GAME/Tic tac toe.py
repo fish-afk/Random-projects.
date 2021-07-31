931db8737d7e62630197016fd3a9a672c81a2720
fish-afk <82692648+fish-afk@users.noreply.github.com>
2021-07-31 15:28:40 +0200

    added a very important feature for fixed x and o placement

--- TIC TAC TOE GAME/Tic tac toe.py
+++ TIC TAC TOE GAME/Tic tac toe.py
@@ -4,6 +4,8 @@ import sys
 from pygame import mixer
 
 
+# display for the start screen and paused screen
+
 def start_screen_display(text, text2, text3):
     text_surface = game_font.render(f'{text}', True, (255, 255, 255))
     text_rect = text_surface.get_rect(center=(200, 120))
@@ -19,19 +21,16 @@ def start_screen_display(text, text2, text3):
     game_screen.blit(text_surface3, text3_rect)
 
 
+# loads the logo on the start screen
+
+
 def load_logo_surfaces(logo1, logo2):
+
     game_screen.blit(logo1, (230, 0))
     game_screen.blit(logo2, (75, 0))
 
 
-def boxes(surface):
-    box_list = [surface for i in range(1, 10)]
-
-    for box in box_list:
-        box_display = pygame.transform.scale(box, (120, 120))
-        box_rect = box_display.get_rect(center=mouse_position)
-        bg_surface.blit(box_display, box_rect)
-
+# handles turns to different players each turn....
 
 def check_the_space(turn):
     global current_player
@@ -50,20 +49,55 @@ def check_the_space(turn):
         x_o_player = font_forXO.render(current_player, True, (0, 0, 0))
 
     if game_on:
-        x_o_rect = x_o_player.get_rect(center=tuple(mouse_position))
+        x_o_rect = x_o_player.get_rect(center=place_x_or_o_at_correct_pos(mouse_position, list_of_coordinates))
         bg_surface.blit(x_o_player, x_o_rect)
 
 
+# all the possible places a person might click will be corrected ...
+
+def place_x_or_o_at_correct_pos(mouse_pos, li_of_coordinates):
+
+    if mouse_pos[0] < 133 and mouse_pos[1] < 133:
+        mouse_pos[0], mouse_pos[1] = li_of_coordinates[0]
+
+    elif 266 > mouse_pos[0] > 133 > mouse_pos[1]:
+        mouse_pos[0], mouse_pos[1] = li_of_coordinates[1]
+
+    elif mouse_pos[0] > 266 and mouse_pos[1] < 133:
+        mouse_pos[0], mouse_pos[1] = li_of_coordinates[2]
+
+    elif mouse_pos[0] < 133 < mouse_pos[1] < 266:
+        mouse_pos[0], mouse_pos[1] = li_of_coordinates[3]
+
+    elif 133 < mouse_pos[0] < 266 and 133 < mouse_pos[1] < 266:
+        mouse_pos[0], mouse_pos[1] = li_of_coordinates[4]
+
+    elif mouse_pos[0] > 266 and mouse_pos[1] > 133 and mouse_pos[1] < 266:
+        mouse_pos[0], mouse_pos[1] = li_of_coordinates[5]
+
+    elif mouse_pos[0] < 133 and mouse_pos[1] > 266:
+        mouse_pos[0], mouse_pos[1] = li_of_coordinates[6]
+
+    elif 133 < mouse_pos[0] < 266 and mouse_pos[1] > 266:
+        mouse_pos[0], mouse_pos[1] = li_of_coordinates[7]
+
+    elif mouse_pos[0] > 266 and mouse_pos[1] > 266:
+        mouse_pos[0], mouse_pos[1] = li_of_coordinates[8]
+
+    return mouse_pos
+
+
 # EXECUTE -
+
 pygame.init()
 mixer.init()
 mixer.music.load("Assets/sfx_point.wav")
 # Setting the volume
 mixer.music.set_volume(0.7)
 
-list_of_coordinates = [(66.666, 66.666), (199.99933, 66.666), (333.333366666, 66.666),  # first row
-                       (66.666, 199.99933), (199.99933, 199.99933), (333.333366666, 199.99933),  # second row
-                       (66.666, 333.33336666), (199.99933, 333.33336666), (333.33336666, 333.33336666)]  # third row
+list_of_coordinates = [[66.666, 66.666], [199.99933, 66.666], [333.333366666, 66.666],  # first row
+                       [66.666, 199.99933], [199.99933, 199.99933], [333.333366666, 199.99933],  # second row
+                       [66.666, 333.33336666], [199.99933, 333.33336666], [333.33336666, 333.33336666]]  # third row
 
 # our surfaces and global variables: ---------------------------------------------------------------
 
@@ -118,7 +152,7 @@ counter = 0
 while run:
 
     for event in pygame.event.get():
-        mouse_position = pygame.mouse.get_pos()  # gets all positions of the cursor as game is running
+        mouse_position = list(pygame.mouse.get_pos())  # gets all positions of the cursor as game is running
         if event.type == pygame.QUIT:
             run = False
             pygame.quit()
@@ -133,11 +167,12 @@ while run:
         if event.type == pygame.MOUSEBUTTONDOWN:  # deploying X and O with the left click
             if event.button == 1 and game_on:
                 counter += 1
-                boxes(box_surface)
+
                 check_the_space(counter)
 
                 if counter < 10:
-                    mixer.music.play()  # sound only plays when counter is less than
+                    mixer.music.play()
+                    # sound only plays when counter is less than
 
                 if counter == 10:  # this sound plays when counter is 10
                     mixer.init()
@@ -152,6 +187,7 @@ while run:
 
     if game_on:  # if game is on, the surface loads, at 0, 0 but if counter goes above 10, game on turns False, and
         # game screen turns black..
+
         game_screen.blit(bg_surface, (0, 0))
         if counter >= 10:
             game_on = False
